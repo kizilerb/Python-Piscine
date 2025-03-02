c51c1e4ca389a59798cbc9136d43e3f7cbac9e92
Berna Kızıler <94437216+Cancelight@users.noreply.github.com>
2025-03-02 14:18:37 +0300

    ex03 is done.

---
+++ python_0/ex03/NULL_not_found.py
@@ -0,0 +1,17 @@
+def NULL_not_found(object: any) -> int:
+    types = {
+        type(None) : [None, "Nothing:"],
+        float : "Cheese:",
+        int : [0, "Zero:"],
+        str : ['', "Empty:"],
+        bool: [False, "Fake:"]
+    }
+    if type(object) in types and (types[type(object)])[0] == object:
+        print((types[type(object)])[1], object, type(object))
+        return (0)
+    elif object != object:
+        print(types[float], object, type(object))
+        return (0)
+    else :
+        print("Type not Found")
+        return 1
