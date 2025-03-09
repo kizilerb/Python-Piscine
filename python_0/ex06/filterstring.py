fbdc3bdf8ac24e2c5636d324726d4e37ce35e76d
Berna Kızıler <94437216+Cancelight@users.noreply.github.com>
2025-03-09 21:42:52 +0300

    norminette is handled, document should be added for evaluation.

--- python_0/ex06/filterstring.py
+++ python_0/ex06/filterstring.py
@@ -1,26 +1,30 @@
 import sys
 from ft_filter import ft_filter
 
+
 def is_num(N):
     try:
         int(sys.argv[2])
-        return(True)
+        return (True)
     except ValueError:
-        return(False)
+        return (False)
+
 
 def filterstring(str):
-    sort_func = lambda input: True if len(input) > int(sys.argv[2]) else False
     separated = str.split()
-    sorted = list(ft_filter(sort_func, separated))
+    num = int(sys.argv[2])
+    sorted = list(ft_filter(lambda i: 1 if len(i) > num else 0, separated))
     print(sorted)
+
 
 def main():
     if len(sys.argv) == 3 and is_num(sys.argv[2]):
         filterstring(sys.argv[1])
     else:
         print("AssertionError: the arguments are bad")
-        return(1)
-    return(0)
+        return (1)
+    return (0)
+
 
 if __name__ == "__main__":
     main()
