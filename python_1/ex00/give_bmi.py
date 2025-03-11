4eedc769ce459ece5fe9f62a03aa7c0dc33cc814
Berna Kızıler <94437216+Cancelight@users.noreply.github.com>
2025-03-11 16:24:16 +0300

    bmi in ex00 is done.

--- python_1/ex00/give_bmi.py
+++ python_1/ex00/give_bmi.py
@@ -1,23 +1,39 @@
-import numpy as np
-import sys
-
-
 def give_bmi(height: list[int | float], weight: list[int | float]) -> list[int | float]:
     bmi_values = []
     try:
-        if not isinstance(height, (list, int)) or not isinstance(weight, (list,int)):
-            raise TypeError("Values shuould be integer or float")
-        elif len(height) != len(weight):
+        if len(height) != len(weight):
             raise ValueError("Number of height and weight inputs should be equal")
-        elif height <= 0 or weight <= 0:
-            raise ValueError("Values of height and weight should be positive.")
-        else:
-            bmi_values.append(weight/(height**2))
+        for h, w in zip(height, weight):
+            if not isinstance(h, (float, int)) or not isinstance(w, (float,int)):
+                raise TypeError("Values shuould be integer or float")
+            if h <= 0 or w <= 0:
+                raise ValueError("Values of height and weight should be positive.")
+            else:
+                bmi_values.append(w/(h**2))
     except Exception as e:
         print("Error:", e)
     return bmi_values
 
 
 def apply_limit(bmi: list[int | float], limit: int) -> list[bool]:
-#your code here
-    
+    limit_check = []
+    try:
+        if not isinstance(limit, int):
+            raise TypeError("Limit should be an integer.")
+        else:
+            limit_check.append(list(True if i > limit else False for i in bmi))
+    except TypeError as e:
+        print("Type error:", e)
+    return (limit_check)
+
+
+def main():
+    height = [2.71, 1.15]
+    weight = [165.3, 38.4]
+    bmi = give_bmi(height, weight)
+    print(bmi, type(bmi))
+    print(apply_limit(bmi, 26))
+
+
+if __name__ == "__main__":
+    main()
