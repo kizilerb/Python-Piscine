43b082e6c0a28cff13e7f350feec0e7895139f12
Berna Kızıler <94437216+Cancelight@users.noreply.github.com>
2025-03-14 15:44:07 +0300

    Some problems occured due to array shape, they're handled

--- python_1/ex03/zoom.py
+++ python_1/ex03/zoom.py
@@ -8,20 +8,25 @@ def zooming(image, h, w):
     # cropping a small region to resize it and give zoomed effect, (left, top, right, bottom)
     cropped = image.crop(((w//2)-150, (h//2)-150, (w//2)+150, (h//2)+150)) 
     zoomed = cropped.resize((400,400))
-    # zoomed.show()
-    img_array = np.array(zoomed)
-    imgplot = plt.imshow(img_array, cmap="gray")
+    # for image information image is covnerted into array format and reshaped from (400,400)
+    img_array = np.array(zoomed).reshape(400,400,1)
+    print(f"New shape after slicing: {img_array.shape}")
+    print(img_array)
+    # plotting and visual output 
+    plt.imshow(img_array, cmap="gray")
     plt.xlabel("X-axis (pixels)")
     plt.ylabel("Y-axis (pixels)")
     plt.title("Zoomed Image")
     plt.show()
+    return (img_array)
 
 
 def main():
     image = ft_load("./animal.jpg")
     print(image)
     # converting image from array to image and in a grayscale format
-    img2zoom = img.fromarray(image).convert("L")
+    img2zoom = img.fromarray(image)
+    img2zoom = img2zoom.convert("L")
     # getting grayscaled image and also original pixel size of the photo to slice
     zooming(img2zoom, len(image), len(image[0]))
 
