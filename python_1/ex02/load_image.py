bc7a8e5ca2de5cfa3552599a4a1e055bf7b77a01
Berna Kızıler <94437216+Cancelight@users.noreply.github.com>
2025-03-14 13:44:25 +0300

    debugging is done, it works as given in the subject

--- python_1/ex02/load_image.py
+++ python_1/ex02/load_image.py
@@ -2,17 +2,18 @@ import PIL.Image as img
 import numpy as np
 import os
 
+
 def ft_load(path: str) -> np.array:
     try:
-        if path.lower().endswith(".jpg", ".jpeg"):
+        if not path.lower().endswith((".jpg", ".jpeg")):
             raise AssertionError("Only JPG and JPEG file formats are handled")
         elif not os.path.exists(path) or not os.path.isfile(path):
             raise AssertionError("Please use a valid file path")
         image = img.open(path)
         img_rgb = image.convert("RGB")
-        print(f"The shape of image is: {image.format}")
-        return (np.array(img.size(img_rgb)))
+        img_array = np.array(img_rgb)
+        print(f"The shape of image is: {img_array.shape}")
+        return (img_array)
     except AssertionError as e:
         print("Error:", e)
-        return(np.array(0))
-
+        return (np.array(0))
---
+++ python_1/ex02/test_img.py
@@ -0,0 +1,4 @@
+from load_image import ft_load
+print(ft_load("landscape.jpeg"))
+print(ft_load("landscape.jppg"))
+print(ft_load("../landscape.jpg"))
