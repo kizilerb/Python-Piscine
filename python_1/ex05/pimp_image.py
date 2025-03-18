f7fb98428025a5fd4229cda39e9f5b734a1df28a
Berna Kızıler <94437216+Cancelight@users.noreply.github.com>
2025-03-18 15:03:53 +0300

    kinda handled

--- python_1/ex05/pimp_image.py
+++ python_1/ex05/pimp_image.py
@@ -1,19 +1,21 @@
 import numpy as np
 import PIL.Image as img
 
+
 def ft_invert(array):
-    inverted = 255 - array.copy()
+    inverted = array.copy()
+    inverted = 255 - inverted
     img.fromarray(inverted).show()
     return (inverted)
 
 
-def ft_red(array): #RGB
+def ft_red(array):
     red_img = array.copy()
     red_img[:, :, 1] *= 0
     red_img[:, :, 2] *= 0
     img.fromarray(red_img).show()
     return (red_img)
-    
+
 
 def ft_green(array):
     green_img = array.copy()
@@ -32,4 +34,11 @@ def ft_blue(array):
 
 
 def ft_grey(array):
-#your code here
+    grey_img = array.copy()
+    ratio = np.array([1/3, 1/3, 1/3])
+    grey_values = np.dot(array[:, :, :3], ratio)
+    grey_img[:, :, 0] = grey_values
+    grey_img[:, :, 1] = grey_values
+    grey_img[:, :, 2] = grey_values
+    img.fromarray(grey_img).show()
+    return (grey_img)
---
+++ python_1/ex05/pimp_tester.py
@@ -0,0 +1,11 @@
+from load_image import ft_load
+from pimp_image import *
+
+array = ft_load("landscape.jpeg")
+print(type(array))
+ft_invert(array)
+ft_red(array)
+ft_green(array)
+ft_blue(array)
+ft_grey(array)
+print(ft_invert.__doc__)
