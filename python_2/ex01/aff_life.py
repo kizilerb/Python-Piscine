1c3e0bd412c22cecbdaeac9446e19e08bf265243
Berna Kızıler <94437216+Cancelight@users.noreply.github.com>
2025-05-02 21:37:25 +0300

    changes in casting

--- python_2/ex01/aff_life.py
+++ python_2/ex01/aff_life.py
@@ -8,13 +8,10 @@ def main():
     campus = data_table[data_table['country'] == 'Turkey']
     print(campus)
     years = list(campus.iloc[0].keys()[1:])
-    #casting into numeric data
     years = [int(y) for y in years]
     age = campus.iloc[0].values[1:]
-    age = [int(a) for a in age]
     plt.figure(1)
     plt.plot(years, age)
-    #set ticks
     tick_years = list(range(min(years), max(years)+1, 40))
     plt.xticks(tick_years)
     plt.xlabel("Year")
