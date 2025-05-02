f4db51a26ea21db3c0dda02295e916173dcc32c6
Berna Kızıler <94437216+Cancelight@users.noreply.github.com>
2025-05-02 17:25:35 +0300

    ex00 is done

---
+++ python_2/ex00/load_csv.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import os
+
+
+def path_check(path: str):
+    try:
+        if not os.path.exists(path) or not os.path.isfile(path):
+            raise AssertionError("File path is wrong.")
+        elif not path.lower().endswith(".csv"):
+            raise AssertionError("File is not in a csv format.")
+        return True
+    except AssertionError as e:
+        print("Error:", e)
+        return False
+
+
+def load(path: str) -> pd.DataFrame:
+    if not path_check(path):
+        return None
+    data = pd.read_csv(path)
+    data_table = pd.DataFrame(data)
+    print("Loading dataset of dimensions", data_table.shape)
+    return (data_table)
