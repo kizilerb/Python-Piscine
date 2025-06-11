d516f0f031a936134fc9477c48f064c2437967af
Berna Kızıler <94437216+Cancelight@users.noreply.github.com>
2025-06-11 16:03:55 +0300

    comment optimization

--- python_0/ex06/ft_filter.py
+++ python_0/ex06/ft_filter.py
@@ -1,12 +1,11 @@
-"""
-filter(function or None, iterable) --> filter object
-
-Return an iterator yielding those items of iterable for which function(item)
-is true. If function is None, return the items that are true.
-"""
-
-
 def ft_filter(function: callable, input: iter):
+    """
+    filter(function or None, iterable) --> filter object
+
+    Return an iterator yielding those items of iterable for which
+    function(item) is true. If function is None, return the items
+    that are true.
+    """
     if function:
         yield from (i for i in input if function(i))
     else:
